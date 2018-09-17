23b74f931693c8a4ce4dd6fc6207382e421707b1
SUNGYU CHO <kt5161497@naver.com>
2018-09-17 17:03:52 +0900

    edit webcafe.js

--- fds11/js/webcafe.js
+++ fds11/js/webcafe.js
@@ -1,8 +1,13 @@
 // jQuery를 활용한 인터렉션 ...
 var item = $('.menu-item');
 var subItem= $('.sub-menu a');
+var tab = $('.tab');
+
+// 메인 메뉴 제어 함수
 item.attr('tabindex','0');
 subItem.attr("class",'fas fa-angle-left');
+tab.attr('tabindex','0');
+
 
 item.on('mouseover focusin', function(){
   item.removeClass('menu-act');
@@ -11,4 +16,9 @@ item.on('mouseover focusin', function(){
 
 subItem.hover(function(){
   $(this).toggleclass('fa-angle-right');
+});
+// 탭 매뉴 제어를 위한 함수
+tab.on('click focusin',function(){
+  $(this).parent().siblings().removeClass('tab-act');
+  $(this).parent().addClass('tab-act');
 });
